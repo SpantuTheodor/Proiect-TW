fe130c648b1296b146804cb72a6daba882ff772f
CalinRogoza <61733873+CalinRogoza@users.noreply.github.com>
2020-05-16 12:07:00 +0300

    Added pop-up for categories page

--- Categories/forg.php
+++ Categories/forg.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,19 +12,22 @@
     <link rel="stylesheet" type="text/css" href="appl.css">
     
 
+
     <title>Forg</title>
 </head>
+
 <body>
     <div class="DIVmare">
         <header class="nav-bar">
             <nav>
                 <a id="a1" href="#"><img class="nav-icon" src="assets/icons/home.png" alt="home-icon">HOME</a>
-                <a id="a2" href="#"><img class="nav-icon" src="assets/icons/trending.png" alt="trending-icon">TRENDING</a>
+                <a id="a2" href="#"><img class="nav-icon" src="assets/icons/trending.png"
+                        alt="trending-icon">TRENDING</a>
                 <a id="a3" href="#"><img class="nav-icon" src="assets/icons/about.png" alt="about-icon">ABOUT</a>
                 <a id="a4" href="#">LOGIN</a>
             </nav>
         </header>
-        
+
         <div>
             <h1> Recommended Categories </h1>
         </div>
@@ -59,121 +63,149 @@
         </section>
 
         <div id="srch">
-                <input type="text" id="search" name="search" placeholder="Search for a recipe..."> 
-                <input id="submit-input" type="submit" name="submit-input" value="Caută!">
-        </div>  
-        
+            <input type="text" id="search" name="search" placeholder="Search for a recipe...">
+            <input id="submit-input" type="submit" name="submit-input" value="Caută!">
+        </div>
+
         <div class="lista">
             <div class="filtre">
                 <h3>Filtre</h3>
                 <label class="container">Fast Food
                     <input type="checkbox">
                     <span class="checkmark"></span>
-                  </label>
-                  
-                  <label class="container">Salate
-                    <input type="checkbox">
-                    <span class="checkmark"></span>
-                  </label>
-                  
-                  <label class="container">Supe și ciorbe
-                    <input type="checkbox">
-                    <span class="checkmark"></span>
-                  </label>
-                  
-                  <label class="container">Aperitive
-                    <input type="checkbox">
-                    <span class="checkmark"></span>
-                  </label>
-                  
-                  <label class="container">Mic Dejun
-                    <input type="checkbox">
-                    <span class="checkmark"></span>
-                  </label>
-                  
-                  <label class="container">Garnituri
-                    <input type="checkbox">
-                    <span class="checkmark"></span>
-                  </label>
-                  
-                  <label class="container">Cu carne
-                    <input type="checkbox">
-                    <span class="checkmark"></span>
-                  </label>
-                  
-                  <label class="container">Murături
-                    <input type="checkbox">
-                    <span class="checkmark"></span>
-                  </label>
+                </label>
 
-                  <label class="container">Gustări
+                <label class="container">Salate
                     <input type="checkbox">
                     <span class="checkmark"></span>
-                  </label>
+                </label>
 
-                  <label class="container">Desert
+                <label class="container">Supe și ciorbe
                     <input type="checkbox">
                     <span class="checkmark"></span>
-                  </label>
+                </label>
+
+                <label class="container">Aperitive
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+
+                <label class="container">Mic Dejun
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+
+                <label class="container">Garnituri
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+
+                <label class="container">Cu carne
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+
+                <label class="container">Murături
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+
+                <label class="container">Gustări
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+
+                <label class="container">Desert
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
             </div>
-        <section class="content">
-            <article id="id1">
-                <div class="poze">   
-                </div>
-                <div class="informatii">
-                    <img title="Add to favorites" class="love_icon" src="assets/icons/favorite_icon.png" alt="add to favorites icon">
-                    <img title="Add to shoppping list" class="love_icon" src="assets/icons/add_to_shopping_list.png" alt="add to shoppping list icon" style="width: 24px; height: 24px;">  
-                    <h2>Somon</h2>
-                    <p>Aceasta reteta este Acee Aceasta ret estta esteAceasta reteta est </p>
-                    <a id="ca1" href="#">Citeste mai mult...</a>
-                </div>
-            </article>
-            <article id="id1">
-                <div class="poze">    
-                </div>
-                <div class="informatii">
-                    <img title="Add to favorites" class="love_icon" src="assets/icons/favorite_icon.png" alt="add to favorites icon">
-                    <img title="Add to shoppping list" class="love_icon" src="assets/icons/add_to_shopping_list.png" alt="add to shoppping list icon" style="width: 24px; height: 24px;">
-                    <h2>Somon</h2>
-                    <p>Aceasta reteta este Acee Aceasta ret esteAceasta reteta esteAceasta reteta esteAceasta reteta este e</p>
-                    <a id="ca1" href="#">Citeste mai mult...</a>
-                </div>
-            </article>
-            <article id="id1">
-                <div class="poze">
-                </div>
-                <div class="informatii">
-                    <img title="Add to favorites" class="love_icon" src="assets/icons/favorite_icon.png" alt="add to favorites icon">
-                    <img title="Add to shoppping list" class="love_icon" src="assets/icons/add_to_shopping_list.png" alt="add to shoppping list icon" style="width: 24px; height: 24px;">
-                    <h2>Somon</h2>
-                    <p>Aceasta reteta este Acee Aceasta ret esteAceasta reteta esteAceasta reteta esteAceasta reteta este</p>
-                    <a id="ca1" href="#">Citeste mai mult...</a>
-                </div>
-            </article>
-            <article id="id1">
-                <div class="poze">   
-                </div>
-                <div class="informatii">
-                    <img title="Add to favorites" class="love_icon" src="assets/icons/favorite_icon.png" alt="add to favorites icon">
-                    <img title="Add to shoppping list" class="love_icon" src="assets/icons/add_to_shopping_list.png" alt="add to shoppping list icon" style="width: 24px; height: 24px;">
-                    <h2>Somon</h2>
-                    <p>Aceasta reteta este Acee Aceasta ret esteAceasta reteta esteAceasta reteta esteAceasta reteta este</p>
-                    <a id="ca1" href="#">Citeste mai mult...</a>
-                </div>
-            </article>
-            <article id="id1">
-                <div class="poze">    
-                </div>
-                <div class="informatii">
-                    <img title="Add to favorites" class="love_icon" src="assets/icons/favorite_icon.png" alt="add to favorites icon">
-                    <img title="Add to shoppping list" class="love_icon" src="assets/icons/add_to_shopping_list.png" alt="add to shoppping list icon" style="width: 24px; height: 24px;">
-                    <h2>Somon</h2>
-                    <p>Aceasta reteta este Acee Aceasta ret esteAceasta reteta esteAceasta reteta esteAceasta reteta este</p>
-                    <a id="ca1" href="#">Citeste mai mult...</a>
-                </div>
-            </article>
-        </section>
+            <section class="content">
+                <article id="id1">
+                    <div class="poze">
+                    </div>
+                    <div class="informatii">
+                        <img title="Add to favorites" class="love_icon" src="assets/icons/favorite_icon.png"
+                            alt="add to favorites icon">
+                        <img title="Add to shoppping list" class="love_icon" src="assets/icons/add_to_shopping_list.png"
+                            alt="add to shoppping list icon" style="width: 24px; height: 24px;">
+                        <h2>Somon</h2>
+                        <p>Aceasta reteta este Acee Aceasta ret estta esteAceasta reteta est </p>
+
+                        <button id="pop-up-button">Citeste mai mult...</button>
+
+                        <!-- The Modal -->
+                        <div id="pop-up" class="popup">
+
+                            <!-- Modal content -->
+                            <div class="pop-up-content">
+                                <span class="close">&times;</span>
+                                <p>Some text in the Modal..</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </article>
+                <article id="id1">
+                    <div class="poze">
+                    </div>
+                    <div class="informatii">
+                        <img title="Add to favorites" class="love_icon" src="assets/icons/favorite_icon.png"
+                            alt="add to favorites icon">
+                        <img title="Add to shoppping list" class="love_icon" src="assets/icons/add_to_shopping_list.png"
+                            alt="add to shoppping list icon" style="width: 24px; height: 24px;">
+                        <h2>Somon</h2>
+                        <p>Aceasta reteta este Acee Aceasta ret esteAceasta reteta esteAceasta reteta esteAceasta reteta
+                            este e</p>
+                        <a id="ca1" href="#">Citeste mai mult...</a>
+                    </div>
+                </article>
+                <article id="id1">
+                    <div class="poze">
+                    </div>
+                    <div class="informatii">
+                        <img title="Add to favorites" class="love_icon" src="assets/icons/favorite_icon.png"
+                            alt="add to favorites icon">
+                        <img title="Add to shoppping list" class="love_icon" src="assets/icons/add_to_shopping_list.png"
+                            alt="add to shoppping list icon" style="width: 24px; height: 24px;">
+                        <h2>Somon</h2>
+                        <p>Aceasta reteta este Acee Aceasta ret esteAceasta reteta esteAceasta reteta esteAceasta reteta
+                            este</p>
+                        <a id="ca1" href="#">Citeste mai mult...</a>
+                    </div>
+                </article>
+                <article id="id1">
+                    <div class="poze">
+                    </div>
+                    <div class="informatii">
+                        <img title="Add to favorites" class="love_icon" src="assets/icons/favorite_icon.png"
+                            alt="add to favorites icon">
+                        <img title="Add to shoppping list" class="love_icon" src="assets/icons/add_to_shopping_list.png"
+                            alt="add to shoppping list icon" style="width: 24px; height: 24px;">
+                        <h2>Somon</h2>
+                        <p>Aceasta reteta este Acee Aceasta ret esteAceasta reteta esteAceasta reteta esteAceasta reteta
+                            este</p>
+                        <a id="ca1" href="#">Citeste mai mult...</a>
+                    </div>
+                </article>
+                <article id="id1">
+                    <div class="poze">
+                    </div>
+                    <div class="informatii">
+                        <img title="Add to favorites" class="love_icon" src="assets/icons/favorite_icon.png"
+                            alt="add to favorites icon">
+                        <img title="Add to shoppping list" class="love_icon" src="assets/icons/add_to_shopping_list.png"
+                            alt="add to shoppping list icon" style="width: 24px; height: 24px;">
+                        <h2>Somon</h2>
+                        <p>Aceasta reteta este Acee Aceasta ret esteAceasta reteta esteAceasta reteta esteAceasta reteta
+                            este</p>
+                        <a id="ca1" href="#">Citeste mai mult...</a>
+                    </div>
+                </article>
+            </section>
         </div>
     </div>
 </body>
+
 </html>
+<script src="categorii.js"></script>
